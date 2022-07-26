1e94f117ef998e5ccd05470c4a043423376c9b7b
kmnguyen436 <80858360+kmnguyen436@users.noreply.github.com>
2022-07-25 19:49:01 -0700

    Add files via upload

---
+++ following_tutorials/CalculatorApp/Calculator/Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Kayla on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
